dcd9ab5cb11785988f0e3100ea45dcae04ed2c48
Oliver Loeffler <22102800+Oliver-Loeffler@users.noreply.github.com>
2021-01-23 18:30:15 +0100

    Added few more actions for testing.

--- trayicon/src/main/java/fxtray/demo/App.java
+++ trayicon/src/main/java/fxtray/demo/App.java
@@ -26,7 +26,10 @@ public class App extends Application {
         TextField textField = new TextField();
         Button showTrayIcon = new Button("Show FXTrayIcon");
         Button hideTrayIcon = new Button("hide FXTrayIcon");
-        Button showErrorMessage = new Button("Show Message in FXTray");
+        Button showErrorMessage = new Button("Error");
+        Button showInfoMessage = new Button("Info");
+        Button showWarningMessage = new Button("Warning");
+        Button showMessage = new Button("Message");
                
         // Layout
         VBox box = new VBox();
@@ -36,20 +39,28 @@ public class App extends Application {
         box.getChildren().add(showTrayIcon);
         box.getChildren().add(hideTrayIcon);
         box.getChildren().add(showErrorMessage);
-                
+        box.getChildren().add(showInfoMessage);
+        box.getChildren().add(showWarningMessage);
+        box.getChildren().add(showMessage);
+        
         // Actions
         FXTrayIcon icon = new FXTrayIcon(primaryStage, getClass().getResource("/Icon.png"));
+        icon.setTrayIconTooltip("This is AWT helping out JavaFX.");
         MenuItem fxMenuItem = new MenuItem("Say Hello from Tray");
         icon.addMenuItem(fxMenuItem);
-        fxMenuItem.setOnAction(event->{
-        	textField.setText("Hello from Sys Tray");
-        });
+        fxMenuItem.setOnAction(event->textField.setText("Hello from Sys Tray"));
+        
+        icon.setOnAction(evt->textField.setText("Double click on icon received."));
+        icon.setOnClick(evt->textField.setText("Got a single click."));
         
         primaryStage.setOnCloseRequest(event->icon.hide());
         showTrayIcon.setOnAction(evt->icon.show());
-        
         hideTrayIcon.setOnAction(evt->icon.hide());
         showErrorMessage.setOnAction(evt->icon.showErrorMessage("Error!", "A message or a stack trace\n or something else."));
+        showInfoMessage.setOnAction(evt->icon.showInfoMessage("Info", "Some info"));
+        showWarningMessage.setOnAction(evt->icon.showWarningMessage("Warning", "Something is wrong.\nCannot tell here!"));
+        showMessage.setOnAction(evt->icon.showMessage("Message", "Two SQL developers meet in a bar.\nUnfortunately they left\nas there were no tables."));
+        
         closeApplication.setOnAction(evt->{
         	if (icon.isShowing())
         		icon.hide();
